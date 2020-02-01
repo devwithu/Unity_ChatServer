5a18e07a61bcbe60e87a63bac3f679409f1295e0
devwithu <devwithu@naver.com>
2020-02-01 18:05:40 +0900

    init

---
+++ Assets/Scripts/Client/Client.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using System.IO;
+
+public class Client : MonoBehaviour
+{
+    public GameObject chatContainer;
+    public GameObject messagePrefab;
+
+    public string clientName;
+
+    private bool socketReady;
+    private TcpClient socket;
+    private NetworkStream stream;
+    private StreamReader reader;
+    private StreamWriter writer;
+
+    public void ConnectToServer() {
+        // If aleady connected, ignore this function
+        if(socketReady)
+            return;
+        // Default host/ port values
+        string host = "127.0.0.1";
+        int port = 6321;
+
+        string h;
+        int p;
+        h = GameObject.Find("HostInput").GetComponent<InputField>().text;
+        if (h != "")
+            host = h;
+        int.TryParse(GameObject.Find("PortInput").GetComponent<InputField>().text, out p);
+        if (p != 0)
+            port = p;
+        
+        // create socket
+        try {
+
+            socket = new TcpClient(host,port);
+            stream = socket.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+
+            socketReady = true;
+
+        }catch(Exception e){
+            Debug.Log("Socket error : " + e.Message);
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(socketReady) {
+            if(stream.DataAvailable) {
+                string data = reader.ReadLine();
+                if(data != null)
+                    OnIncomingData(data);
+            }
+        }
+    }
+    private void OnIncomingData(string data){
+
+        if(data == "%NAME") {
+            Send("&NAME|" + clientName);
+            return;
+        }
+        GameObject go = Instantiate(messagePrefab,chatContainer.transform);
+        go.GetComponentInChildren<Text>().text = data;
+    }
+
+    private void Send(string data) {
+        if(!socketReady)
+            return;
+        
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSendButton() {
+        string message = GameObject.Find("SendInput").GetComponent<InputField>().text;
+        Send(message);
+    }
+
+    public void CloseSocket() {
+        if(!socketReady)
+            return;
+        
+        writer.Close();
+        reader.Close();
+        socket.Close();
+        socketReady = false; 
+
+    }
+
+    private void OnApplicationQuit() {
+        CloseSocket();
+    }
+
+    void OnDisable()
+    {
+        CloseSocket();
+    }
+}
---
+++ Assets/Scripts/Server/Server.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+using System.Net;
+using System.IO;
+
+public class Server : MonoBehaviour
+{
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectList;
+
+    public int port = 6321;
+    private TcpListener server;
+    private bool serverStarted;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clients = new List<ServerClient>();
+        disconnectList = new List<ServerClient>();
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            StartListening();
+            serverStarted = true;
+            Debug.Log("Server has been stated on port " + port);
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptTcpClient(AcceptTcpClient,server);
+    }
+
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener) ar.AsyncState;
+        clients.Add(new ServerClient(listener.EndAcceptTcpClient(ar)));
+        StartListening();
+
+        // Send a message to everyone , day someone has connected
+        //Broadcast(clients[clients.Count-1].clientName + " has connected", clients);
+        Broadcast("%NAME", new List<ServerClient>() { clients[clients.Count - 1]});
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(!serverStarted)
+            return;
+
+        foreach(ServerClient c in clients)
+        {
+            // IsInvoking the client still connected:
+            if(!isConnected(c.tcp))
+            {
+                c.tcp.Close();
+                disconnectList.Add(c);
+                continue;
+            }
+            else {
+                try{
+                    NetworkStream s = c.tcp.GetStream();
+                
+                    if(s.DataAvailable) {
+                        StreamReader reader = new StreamReader(s, true);
+                        string data = reader.ReadLine();
+
+                        if(data != null) {
+                            OnIncomingData(c, data);
+                        }
+                    }
+                }catch (ObjectDisposedException e){
+                    c.tcp.Close();
+                    disconnectList.Add(c);
+                    Debug.Log("c.tcp.GetStream() error : " + e.Message);
+                    continue;
+                }catch (Exception e) {
+                    c.tcp.Close();
+                    disconnectList.Add(c);
+                    Debug.Log("c.tcp.GetStream() error : " + e.Message);
+                    continue;
+                }
+
+            }
+            // check for maeedage from the client
+        }
+
+        for(int i = 0; i < disconnectList.Count -1; i++) {
+            Broadcast(disconnectList[i].clientName + " has disconnected" , clients);
+            
+            clients.Remove(disconnectList[i]);
+            disconnectList.RemoveAt(i);
+        }
+    }
+
+    private void OnIncomingData(ServerClient c, string data) {
+
+        if (data.Contains("&NAME|")) {
+            c.clientName = data.Split('|')[1];
+            Broadcast(c.clientName + " has connected", clients);
+            return;
+        }
+        //Debug.Log(c.clientName + " has send the following message : " + data);
+        Broadcast(c.clientName + " : " + data, clients);
+    }
+    private void Broadcast(string data, List<ServerClient> cl) {
+
+        foreach(ServerClient c in cl) {
+            try {
+                StreamWriter writer = new StreamWriter(c.tcp.GetStream());
+                writer.WriteLine(data);
+                writer.Flush();
+
+            } catch(Exception e) {
+                Debug.Log("Write error : " + e.Message + "to client " + c.clientName);
+            }
+        }
+    }
+    private bool isConnected(TcpClient c)
+    {
+        try
+        {
+            if(c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                {
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0); 
+
+                }
+                return true;
+            }
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
+
+
+
+public class ServerClient{
+    public TcpClient tcp;
+    public string clientName;
+    public ServerClient(TcpClient clientSocket)
+    {
+        clientName = "Guest";
+        tcp = clientSocket;
+    }
+}
